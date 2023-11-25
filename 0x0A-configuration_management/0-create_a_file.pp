bcfbfca076f383c7f3bf05181328a4fbadc8d197
Gabogogi <bencitymaina@gmail.com>
2023-11-25 08:11:33 -0800

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #creates a file in tmp, sets mode, users and permissions
-file {'tmp/school':
-	ensure => 'present',
-	content => 'I love Puppet\n',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
+file { 'tmp/school':
+  ensure  => present,
+  content => 'I love Puppet\n',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }

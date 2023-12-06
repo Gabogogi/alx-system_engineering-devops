6e66ae160c5f0ba9260727def23031e2dfed6322
Gabogogi <bencitymaina@gmail.com>
2023-12-05 22:17:05 -0800

    correction to 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,19 @@
-#Using Puppet, install flask from pip3
-package { 'flask' :
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+# Install flask using pip3
+
+package { 'python3':
+  ensure => present
+}
+
+class { 'python':
+  version => 'system'
+}
+
+package { 'python3-pip':
+    ensure  => present
+}
+
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require  => Package['python3-pip']
 }

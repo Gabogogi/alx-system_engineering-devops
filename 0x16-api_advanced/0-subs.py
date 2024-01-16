b96b5787a6b9df09466b42b72e874aec4aea2eff
Benson Maina <117763241+Gabogogi@users.noreply.github.com>
2024-01-16 12:49:34 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,9 +15,11 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
 
-    if res.status_code == 200:
-        # parse json reponse
-        subreddit_data = res.json()
-        return (subreddit_data['data']['subscribers'])
-    else:
+   if res.status_code != 200:
         return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']

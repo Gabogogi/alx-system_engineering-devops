e73874cb7875871394d81fba31fc7c10d08691df
Gabogogi <bencitymaina@gmail.com>
2024-01-17 08:03:26 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import requests
 """Module for task 0"""
+import requests
+
 
 def number_of_subscribers(subreddit):
     '''
-    A function that queries the Reddit API and returns the number of subscribers,
+    Queries the Reddit API and returns the number of subscribers,
     0 if the subreddit is not valid.
     '''
     headers = {'User-Agent': 'MyAPIReddit'}

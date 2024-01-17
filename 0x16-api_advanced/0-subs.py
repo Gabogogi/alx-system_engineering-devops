02bfa86d5000a3483d2079047dea68480bad015e
Gabogogi <bencitymaina@gmail.com>
2024-01-17 07:57:54 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+import requests
 """Module for task 0"""
 
-
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    '''
+    A function that queries the Reddit API and returns the number of subscribers,
+    0 if the subreddit is not valid.
+    '''
+    headers = {'User-Agent': 'MyAPIReddit'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    res = requests.get(url, headers=headers, allow_redirects=False)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    if res.status_code == 200:
+        sub_data = res.json().get('data', {})
+        return sub_data.get('subscribers', 0)
+    else:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
